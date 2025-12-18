b71db334c1656d6fca29c41b0ced78e90034eebf
Arnab Singha Roy <arnabsr0702@gmail.com>
2025-12-18 10:08:28 +0530

    adding clerk authentication

--- backend/models/user.js
+++ backend/models/user.js
@@ -11,11 +11,6 @@ const userSchema = new mongoose.Schema(
       required: true,
       unique: true,
     },
-    password: {
-      type: String,
-      required: true,
-      select: false,
-    },
     role: {
       type: String,
       enum: ["student", "teacher", "admin"],
---
+++ frontend/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div >
+      <main>
+         <h1>Hello welcome to next js</h1>
+      </main>
+    </div>
+  );
+}
